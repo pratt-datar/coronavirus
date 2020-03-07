90fa45ea4d431e85375edcdb4f16c9c9a3854049
prattdatar <pratt.datar@gmail.com>
2020-03-07 16:56:05 -0500

    fetch file updated again

--- data_fetch.py
+++ data_fetch.py
@@ -5,6 +5,6 @@ cluster = MongoClient("mongodb://localhost:27017/")
 db = cluster["coronavirus_5e332abb06036301c3a1d517"]
 tweets = db["tweets"]
 
-x= mycol.find_one()
+x= tweets.find_one()
 
 print(x)
