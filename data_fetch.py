b216cc3037d1f7498f679c959b5f3fa468527c1d
prattdatar <pratt.datar@gmail.com>
2020-03-17 17:35:12 -0400

    printing tweet v7

--- data_fetch.py
+++ data_fetch.py
@@ -10,8 +10,10 @@ except:
 
 db = cluster["coronavirus_5e332abb06036301c3a1d517"]
 tweets = db["tweets"]
-x = list(tweets.find().limit(5))
-print(x)
+for x in tweets.find({},{,"timestamp_ms":1,"id_str":1,"user.id":1}).limit(5)
+	print(x)
 print("X DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-y = list(tweets.find_one())
-print(y)
+#y = list(tweets.find_one())
+#print(y)
+
+#"entities.hashtags.text":1
