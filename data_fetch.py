89b5e19ddcabdee1d0d9bf5497b44651b54091fc
prattdatar <pratt.datar@gmail.com>
2020-03-17 17:50:50 -0400

    printing hashtags and timestamp differently

--- data_fetch.py
+++ data_fetch.py
@@ -10,9 +10,11 @@ except:
 
 db = cluster["coronavirus_5e332abb06036301c3a1d517"]
 tweets = db["tweets"]
-for x in tweets.find({},{"entities.hashtags.text":1,"timestamp_ms":1,"id_str":1,"user.id":1}).limit(5):
+for x in tweets.find({},{"timestamp_ms":1,"id_str":1,"user.id":1}).limit(5):
 	print(x)
 print("X DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 #y = list(tweets.find_one())
 #print(y)
 
+for y in tweets.find({},{"$and":[ {"entities.hashtags.text":{"$exists": True}}, {"entities.hashtags.text":{"$ne": ""}}]}).limit(5):
+	print(y)
