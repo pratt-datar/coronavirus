6b1578fc78d6c9f79d8cf023280071762ef89f57
prattdatar <pratt.datar@gmail.com>
2020-03-17 04:13:56 -0400

    changes in data mongo auth v2

--- data_fetch.py
+++ data_fetch.py
@@ -3,16 +3,18 @@ from pymongo import MongoClient
 import urllib.parse
 
 username = urllib.parse.quote_plus('pdatar')
-password = urllib.parse.quote_plus('pass/word')
+password = urllib.parse.quote_plus('joshist800')
 
 try:
 
 	cluster = MongoClient('mongodb://%s:%s@127.0.0.1' % (username, password))
+	print("M1 success")
 except:
 	print("Percent-Escaping Username and Password failed")
 
 try:
-	cluster = MongoClient(mongodb://pdatar:joshist800@localhost/coronavirus_5e332abb06036301c3a1d517?authSource=admin)
+	cluster = MongoClient('mongodb://pdatar:joshist800@localhost/coronavirus_5e332abb06036301c3a1d517?authSource=admin')
+	print("M2 Success")
 except:
 	print("stackoverflow method failed")
 
