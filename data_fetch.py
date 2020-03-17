1cc09a1ba82e5ade2b2472f8a212d6432116bdc7
prattdatar <pratt.datar@gmail.com>
2020-03-17 16:27:41 -0400

    printing tweet v3

--- data_fetch.py
+++ data_fetch.py
@@ -1,20 +1,15 @@
 # Imports
 from pymongo import MongoClient
-
+import pprint
 try:
 	cluster = MongoClient('mongodb://bitslab:0rang3!@localhost/?authSource=admin')
 	print("Authentication Success")
 except:
 	print("Authentication Failed")
 
-try:
 
-		db = cluster["coronavirus_5e332abb06036301c3a1d517"]
-		tweets = db["tweets"]
-
-		x= tweets.find_one()
-
-		for document in x: 
-			pprint(document)
-except:
-	print("Try again")
+db = cluster["coronavirus_5e332abb06036301c3a1d517"]
+teets = db["tweets"]
+x tweets.find_one()
+for document in x: 
+	pprint(document)
