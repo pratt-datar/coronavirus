37e0bc7d6db41a00d4cb7154c9b78a06afe55ad5
prattdatar <pratt.datar@gmail.com>
2020-03-22 03:47:51 -0400

    Dry run on Bios Collection

--- data_fetch.py
+++ data_fetch.py
@@ -1,20 +1,21 @@
 # Imports
 from pymongo import MongoClient
-import pprint
+
+# Mongo Authentication
 try:
 	cluster = MongoClient('mongodb://bitslab:0rang3!@localhost/?authSource=admin')
-	print("Authentication Success")
+	print("We are in!")
 except:
 	print("Authentication Failed")
 
-
 db = cluster["coronavirus_5e332abb06036301c3a1d517"]
 tweets = db["tweets"]
-for x in tweets.find({},{"timestamp_ms":1,"id_str":1,"user.id":1}).limit(5):
-	print(x)
-print("X DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-#y = list(tweets.find_one())
-#print(y)
+bios = db['bios']
+#tweet_obj = tweets.find({$or:[{"retweeted_status.extended_tweet.entities.hashtags.text":{$exists:true}},{"entities.hashtags.text":{$exists:true}}]},{"user.screen_name":1,"retweeted_status.user.screen_name":1,"entities.hashtags.text":1,"retweeted_status.extended_tweet.entities.hashtags.text":1,"timestamp_ms":1})
+#print("Tweet Query DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 
-for y in tweets.find({},{"$and":[ {"entities.hashtags.text":{"$exists": True}}, {"entities.hashtags.text":{"$ne": ""}}]}).limit(1):
-	print(y)
+bios_obj = bios.find({$or:[{"awards.award":{$exists:true}},{"death":{$exists:true}}]},{"name.last":1})
+
+for x in bios_obj:
+	print(x)
+
