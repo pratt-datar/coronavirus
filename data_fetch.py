886725f5ad81331ba53bc36c0923c7c6d05bc3bc
Prathamatesh Datar <pdatar@portland.ischool.syr.edu>
2020-03-22 21:53:32 -0400

    csv file changes

--- data_fetch.py
+++ data_fetch.py
@@ -1,21 +1,55 @@
 # Imports
+import json
 from pymongo import MongoClient
+#from bson.json_util import dumps
+import csv
 
 # Mongo Authentication
-try:
-	cluster = MongoClient('mongodb://bitslab:0rang3!@localhost/?authSource=admin')
-	print("We are in!")
-except:
-	print("Authentication Failed")
 
+cluster = MongoClient('mongodb://bitslab:0rang3!@localhost/?authSource=admin')
+
+# database selection
 db = cluster["coronavirus_5e332abb06036301c3a1d517"]
+
+#collection selection
 tweets = db["tweets"]
 bios = db['bios']
-#tweet_obj = tweets.find({$or:[{"retweeted_status.extended_tweet.entities.hashtags.text":{$exists:true}},{"entities.hashtags.text":{$exists:true}}]},{"user.screen_name":1,"retweeted_status.user.screen_name":1,"entities.hashtags.text":1,"retweeted_status.extended_tweet.entities.hashtags.text":1,"timestamp_ms":1})
+
+# QUERY
+tweet_obj = tweets.find({"$or":[{"retweeted_status.extended_tweet.entities.hashtags.text":{"$exists":True}},{"entities.hashtags.text":{"$exists":True}}]},{"user.screen_name":1,"entities.hashtags.text":1,"retweeted_status.extended_tweet.entities.hashtags.text":1,"timestamp_ms":1,"_id":0}).limit(1)
 #print("Tweet Query DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 
-bios_obj = bios.find({$or:[{"awards.award":{$exists:true}},{"death":{$exists:true}}]},{"name.last":1})
+#with open('tweets_new.txt', 'w') as outfile:
+#	json.dump(tweet_obj, outfile)
 
-for x in bios_obj:
-	print(x)
+bios_obj = bios.find({"$or":[{"awards.award":{"$exists":True}},{"death":{"$exists":True}}]},{"name.last":1})
+
+#for x in tweet_obj:
+#	print(x)
+
+#with open('bios.csv', 'w') as outfile:
+#	fields = ['_id', 'name.last']
+#	write = csv.DictWriter(outfile, fieldnames=fields)
+#	write.writeheader()
+#	for records in bios_obj:  # Here we are using 'bios_obj' as an iterator
+#		user_id = records['_id']
+#		last_name = records['name']['last']
+#		#for x in records['name']:
+#		flattened_record = {'_id': user_id,'name.last': last_name}
+#		write.writerow(flattened_record)
+
+text_rt = []
+text_hash = []
+with open('tweets.csv', 'w') as outfile:
+	fields = ['screen_name','timestamp','rt_hashtags']
+	write = csv.DictWriter(outfile, fieldnames=fields)
+	write.writeheader()
+	for records in tweet_obj:  # Here we are using 'tweet_obj' as an iterator
+		username = records['user']['screen_name']
+		timestamp = records['timestamp_ms']
+		for x in records['retweeted_status']['extended_tweet']['entities']['hashtags']:
+			text_rt.append(x['text'])
+		flattened_record = {'screen_name':username,'timestamp': timestamp, 'rt_hashtags': text_rt}
+		write.writerow(flattened_record)
+
 
