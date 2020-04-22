fb8b99ae688022fed1b9acbb5d20ef22ddc07485
Prathamatesh Datar <pdatar@portland.ischool.syr.edu>
2020-04-21 20:32:04 -0400

    updated data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -13,10 +13,15 @@ db = cluster["coronavirus_5e332abb06036301c3a1d517"]
 
 #collection selection
 tweets = db["tweets"]
-bios = db['bios']
+#bios = db['bios']
 
 # QUERY
-tweet_obj = tweets.find({"$or":[{"retweeted_status.extended_tweet.entities.hashtags.text":{"$exists":True}},{"entities.hashtags.text":{"$exists":True}}]},{"user.screen_name":1,"entities.hashtags.text":1,"retweeted_status.extended_tweet.entities.hashtags.text":1,"timestamp_ms":1,"_id":0})
+#tweet_obj = tweets.find({"$or":[{"retweeted_status.extended_tweet.entities.hashtags.text":{"$exists":True}},{"entities.hashtags.text":{"$exists":True}}]},{"user.screen_name":1,"entities":1,"retweeted_status":1,"timestamp_ms":1,"_id":0})
+
+tweet_obj = tweets.find({"$or":[{"retweeted_status.extended_tweet.entities.hashtags.text":{"$exists":True}},{"entities.hashtags.text":{"$exists":True}},{"entities.user_mentions.id":{"$exists":True}},{"retweeted_status.extended_tweet.entities.user_mentions.id":{"$exists":True}}]},{"_id":1,"id":1,"timestamp_ms":1,"entities.user_mentions.id":1,"retweeted_status.id":1,"retweeted_status.extended_tweet.entities.user_mentions.id":1,"retweeted_status.user.id":1,"user.id":1,"entities.hashtags.text":1,"retweeted_status.extended_tweet.entities.hashtags.text":1})
+
+
+
 #print("Tweet Query DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 
 #with open('tweets_new.txt', 'w') as outfile:
@@ -38,24 +43,48 @@ tweet_obj = tweets.find({"$or":[{"retweeted_status.extended_tweet.entities.hasht
 #               flattened_record = {'_id': user_id,'name.last': last_name}
 #               write.writerow(flattened_record)
 
-with open('tweets.csv', 'w') as outfile:
-	fields = ['screen_name','timestamp','rt_hashtags','hashtags']
+with open('tweets_updated_v1.csv', 'w') as outfile:
+	fields = ['_id','tweet_id','rt_tweetid','user','rt_user','timestamp','hashtags','rt_hashtags','mentions','rt_mentions']
 	write = csv.DictWriter(outfile, fieldnames=fields)
 	write.writeheader()
 	for records in tweet_obj:  # Here we are using 'tweet_obj' as an iterator
-		text_rt = []
-		text_hashtags = []
-		username = records['user']['screen_name']
+		rt_hashtags = []
+		hashtags = []
+		rt_mentions = []
+		mentions = []
+		user = records['user']['id']
 		timestamp = records['timestamp_ms']
+		unique_id = records['_id']
+		tweet_id = records['id']
+		try:
+			rt_tweetid = records['retweeted_status']['id']
+		except:
+			rt_tweetid = []
+		try:
+			rt_user = records['retweeted_status']['user']['id']
+		except:
+			rt_user = []
 		try:
 			for x in records['retweeted_status']['extended_tweet']['entities']['hashtags']:
-				text_rt.append(x['text'])
+				rt_hashtags.append(x['text'])
 		except:
-			text_rt = None
+			rt_hashtags = []
 		try:
 			for y in records['entities']['hashtags']:
-				text_hashtags.append(y['text'])
+				hashtags.append(y['text'])
 		except:
-			text_hashtags = None
-		flattened_record = {'screen_name':username,'timestamp': timestamp, 'rt_hashtags': text_rt, 'hashtags': text_hashtags}
+			hashtags = []
+		
+		try:
+			for z in records['retweeted_status']['extended_tweet']['entities']['user_mentions']:
+				rt_mentions.append(z['id'])
+		except:
+			rt_mentions = []
+		try:
+			for w in records['entities']['user_mentions']:
+				mentions.append(w['id'])
+		except:
+			mentions = []
+	
+		flattened_record = {'_id': unique_id, 'tweet_id': tweet_id, 'rt_tweetid': rt_tweetid, 'user': user, 'rt_user': rt_user, 'timestamp': timestamp, 'hashtags': hashtags, 'rt_hashtags': rt_hashtags, 'mentions': mentions, 'rt_mentions': rt_mentions}
 		write.writerow(flattened_record)
